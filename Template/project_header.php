bc78e2f3c8cf5da8ffbe7e4d62de050526f9da6f
root <Nyannyanmiau>
2021-10-25 13:04:43 +0000

    minor fixes

--- Template/project_header.php
+++ Template/project_header.php
@@ -1,5 +1,6 @@
 <div class="project-header <?= $filters['plugin'] && gettype($filters['plugin']) == 'array' ? 'color-' : '' ?><?= $filters['plugin'] && gettype($filters['plugin']) == 'array' ? $filters['plugin']['task']['color_id'] : '' ?>">
-    <?php $filters['plugin'] = '' ?>
+    <?php   // why was this? This prevents the filter work in gantt view AND OTHER EXT using the FILTER!
+            //$filters['plugin'] = '' ?>
 
     <?= $this->hook->render('template:project:header:before', array('project' => $project)) ?>
 
