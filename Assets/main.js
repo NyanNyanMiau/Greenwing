bf5b7489944dd0a7d929fc0bf00de1dfe0aeaaf5
root <Nyannyanmiau>
2021-11-16 18:19:52 +0000

    add loader to all form.submit events

--- Assets/main.js
+++ Assets/main.js
@@ -1,5 +1,15 @@
 jQuery(document).ready(function(){
 
+	// should catch all norm forms not modal ones
+	$(document).on('submit', 'form', function() {
+	    var btnEl = $(this).find('[type="submit"]').prop('disabled', true);
+	    var l = $('<i class="fa fa-spinner fa-pulse me-2"></i> ');
+	    if (btnEl.is('input')) {
+	    	btnEl.val('loading ...');
+	    } else {
+	    	btnEl.prepend(l);
+	    }
+	});
 
 });
 
